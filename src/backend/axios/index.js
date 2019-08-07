ae90081a422e3f2e1dca8f7dc00714db306f969a
dcheley <danielcheley@hotmail.com>
2019-08-07 19:24:00 -0400

    Setting heroku url

--- src/backend/axios/index.js
+++ src/backend/axios/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:3000'
+const API_URL = 'https://merchant-dashboard-backend.herokuapp.com'
 
 const securedAxiosInstance = axios.create({
   baseURL: API_URL,
