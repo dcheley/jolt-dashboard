d379d60144d72eb0e0f9b895852cc43ef01e8759
dcheley <danielcheley@hotmail.com>
2019-10-30 10:07:23 -0400

    Starting to setup vuex-store

---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
+
+  modules: {
+
+  },
+
+  plugins: [createPersistedState({
+    paths: [
+      
+    ]
+  })]
+
+})
